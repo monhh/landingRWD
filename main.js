b9bfcf882ea760503931050f833fa223d288f470
Mon Pampín <mon.medialab@gmail.com>
2024-07-23 20:58:09 +0200

    header navbar

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-/* import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");/* wrap hamburger & menu */
+const primaryNav = document.querySelector(".primary-navigation"); /* wrap ul */
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+navToggle.addEventListener("click", () => {
+	primaryNav.hasAttribute("data-visible")
+		? navToggle.setAttribute("aria-expanded", false)
+		: navToggle.setAttribute("aria-expanded", true);
+	primaryNav.toggleAttribute("data-visible");
+	primaryHeader.toggleAttribute("data-overlay");
+});
 
-setupCounter(document.querySelector('#counter'))
- */
